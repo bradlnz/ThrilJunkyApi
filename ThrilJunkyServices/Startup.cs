85475d19cb0dc831fe6f90c6f2aeb046f4122ceb
thriljunky <brad@thriljunky.com>
2018-02-01 11:00:23 +1300

    Update Startup.cs

--- ThrilJunkyServices/Startup.cs
+++ ThrilJunkyServices/Startup.cs
@@ -34,7 +34,6 @@ namespace ThrilJunkyServices
             {
                 options.Authority = domain;
                 options.ApiName = "customAPI";
-                options.RequireHttpsMetadata = false;
             });
 
             services.AddMvc();
