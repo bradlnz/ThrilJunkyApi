74091a5e0654eb5a95b3d258ad7d660c6a5a0816
thriljunky <brad@thriljunky.com>
2018-02-01 13:00:27 +1300

    Create MediaController.cs

---
+++ ThrilJunkyServices/Controllers/MediaController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ThrilJunkyServices.Models;
+using ThrilJunkyServices.Repositories;
+
+namespace ThrilJunkyServices.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class MediaController : Controller
+    {
+
+        private readonly IMediaRepository mediaRepository;
+
+        public MediaController(IMediaRepository _mediaRepository)
+        {
+            mediaRepository = _mediaRepository;
+        }
+
+        [HttpGet]
+
+        public IActionResult List()
+        {
+            return Ok(mediaRepository.GetAll());
+        }
+
+        [HttpGet("{Id}")]
+        public Media GetItem(int id)
+        {
+            Media item = mediaRepository.GetByID(id);
+
+            return item;
+        }
+
+        [HttpPost]
+        public IActionResult Modify([FromBody]Media item)
+        {
+            try
+            {
+                if (item == null || !ModelState.IsValid)
+                {
+                    return BadRequest("Invalid State");
+                }
+
+                mediaRepository.AddOrUpdate(item);
+
+            }
+            catch (Exception)
+            {
+                return BadRequest("Error while creating");
+            }
+            return Ok(item);
+        }
+
+      [HttpPost]
+      public string UploadMedia(HttpPostedFileBase file)
+      {
+        return mediaRepository.Upload(_config["ConnectionStrings:DefaultConnection"], _config["BlobContainerName"], $"{DateTime.Now}.mp4", file);
+      }
+    }
+}
