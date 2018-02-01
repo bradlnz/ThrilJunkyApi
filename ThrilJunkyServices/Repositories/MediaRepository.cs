5cd4cc8a88595bdfe8608f33d0ef30df1a54d692
thriljunky <brad@thriljunky.com>
2018-02-01 13:13:12 +1300

    Update MediaRepository.cs

--- ThrilJunkyServices/Repositories/MediaRepository.cs
+++ ThrilJunkyServices/Repositories/MediaRepository.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Web;
 using System.Collections.Generic;
 using NPoco;
 using ThrilJunkyServices.Models;
@@ -63,7 +62,7 @@ namespace ThrilJunkyServices.Repositories
             string connectionString,
             string containerName,
             string blobName,
-            HttpPostedFileBase file)
+            IFormFile file)
         {
             var cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
             var cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
