e20c862a9f2e016acc5a2905c547172305eae1cd
thriljunky <brad@thriljunky.com>
2018-02-01 13:17:07 +1300

    Update MediaController.cs

--- ThrilJunkyServices/Controllers/MediaController.cs
+++ ThrilJunkyServices/Controllers/MediaController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ThrilJunkyServices.Models;
 using ThrilJunkyServices.Repositories;
-
+using Microsoft.AspNetCore.Http.Internal.FormFile;
 namespace ThrilJunkyServices.Controllers
 {
     [Route("api/[controller]")]
