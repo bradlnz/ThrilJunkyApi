80af56f930a26f6f33d8fd635f4486850615de14
Brad Lietz <brad@thriljunky.com>
2018-02-12 19:20:02 +0000

    Add Hint Repo

---
+++ ThrilJunkyServices/Repositories/HintRepository.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using NPoco;
+using ThrilJunkyServices.Models;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ThrilJunkyServices.ViewModels;
+using System.Linq;
+
+namespace ThrilJunkyServices.Repositories
+{
+    public class HintRepository : IHintRepository
+    {
+      
+        private  readonly IConfiguration _config;
+
+        public HintRepository(IConfiguration config) 
+        {
+            _config = config;
+        }
+
+        public IDatabase Connection
+        {
+            get
+            {
+                return new Database(_config["ConnectionStrings:DefaultConnection"], DatabaseType.SqlServer2008, SqlClientFactory.Instance);
+            }
+        }
+
+        public async Task Add(Hint hint)
+        {
+
+            using (IDatabase db = Connection)
+            {
+                await db.InsertAsync<Hint>(hint);
+            }
+        }
+
+        public List<Hint> GetAll()
+        {
+            using (IDatabase db = Connection)
+            {
+                return db.Fetch<Hint>("SELECT * FROM Hint");
+            }
+        }
+
+        public Hint GetByID(int id)
+        {
+            using (IDatabase db = Connection)
+            {
+                return db.SingleById<Hint>(id);
+            }
+        }
+
+    }
+}
---
+++ ThrilJunkyServices/Repositories/IHintRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThrilJunkyServices.Models;
+
+namespace ThrilJunkyServices.Repositories
+{
+    public interface IHintRepository
+    {
+        Task Add(Hint hint);
+        List<Hint> GetAll();
+        Hint GetByID(int id);
+    }
+}
