5f8e77a469c4b98baf188ca0b3bd4f51143e4aa4
Unknown <bradleylietz@gmail.com>
2018-02-23 04:36:27 +0000

    Update

--- ThrilJunkyServices/Models/User.cs
+++ ThrilJunkyServices/Models/User.cs
@@ -5,7 +5,7 @@ namespace ThrilJunkyServices.Models
 {
     public class User
     {
-        public string Id { get; set; }
+        public Guid Id { get; set; }
 
         public int MediaId { get; set; }
 
